02a7535fd8a4322f06fa535435956c1ab2f972dd
matibza <123789361+matibza@users.noreply.github.com>
2023-05-15 01:33:49 -0300

    Update ini_calculadora.js

--- ini_calculadora.js
+++ ini_calculadora.js
@@ -18,7 +18,6 @@ var selector = document.getElementById("sel");
 		alert("Cargando turno D");
 	}
 	if (selector.value == "DIUR") {
-		cargar_2023_diurno();
-		alert("Cargando Diurno");
+		alert("Diurno");
 	}
 }
